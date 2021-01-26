b59e5f54a459deeeadae73f401fc1623ed52a959
Elias Kalkhofer <elias.kalkhofer@htl-wels.at>
2021-01-26 11:24:49 +0100

    Interface Design finished

--- src/main/TheMain.java
+++ src/main/TheMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class TheMain extends Application {
@@ -12,6 +13,7 @@ public class TheMain extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/viewcontrol/ViewColorCalculator.fxml"));
         primaryStage.setTitle("Color Calculator");
+        primaryStage.getIcons().add(new Image("/images/ColorWheel.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
